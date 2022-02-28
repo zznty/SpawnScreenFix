c665e4bd89430c0eeebec79f72384a2d10c65743
z__ <vinduus585@gmail.com>
2022-02-28 22:21:30 +0700

    first

---
+++ SpawnScreenFix/Plugin.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using VRage.Plugins;
+
+namespace SpawnScreenFix
+{
+    public class Plugin : IPlugin
+    {
+        public void Dispose()
+        {
+        }
+
+        public void Init(object gameInstance)
+        {
+            new Harmony(typeof(Plugin).Namespace).PatchAll(typeof(Plugin).Assembly);
+        }
+
+        public void Update()
+        {
+        }
+    }
+}
---
+++ SpawnScreenFix/RespawnScreenPatch.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+using System.Text;
+using HarmonyLib;
+using Sandbox.Game.Localization;
+using Sandbox.Graphics.GUI;
+using SpaceEngineers.Game.GUI;
+using VRage;
+
+namespace SpawnScreenFix
+{
+    [HarmonyPatch]
+    internal static class RespawnScreenPatch
+    {
+        private static MethodBase TargetMethod()
+        {
+            return AccessTools.FirstMethod(typeof(MyGuiScreenMedicals),
+                m => m.Name.Contains("<RefreshMedicalRooms>g__AddSuitRespawn"));
+        }
+
+        private static void Postfix(MyGuiControlTable ___m_respawnsTable)
+        {
+            var row = ___m_respawnsTable.Find(b => b.GetCell(0).Text.EqualsStrFast(MyTexts.GetString(MySpaceTexts.SpawnInSpaceSuit)));
+            if (row is null)
+                return;
+            ___m_respawnsTable.Remove(row);
+            ___m_respawnsTable.Insert(0, row);
+            ___m_respawnsTable.SelectedRow = row;
+        }
+    }
+}
